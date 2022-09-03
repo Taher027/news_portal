f85c8364bdea459b708a42345bc677870764641c
Taher027 <abutaherh27@gmail.com>
2022-09-03 09:24:21 +0600

    added category menu daynamically

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-const loadAllBlog = async() =>{
-    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
-    const data = await response.json();
-    return data.data;
-}
+const loadAllBlog = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    try {
+        const res = await fetch(url);
+        const data = await res.json()
+        return (data.data.news_category)
+    }
+    catch(error){
+        console.log(error)
+    }
+};
 const setAllMenu = async() => {
     const data = await loadAllBlog();
-    // console.log(data.news_category[5].category_name)
-    const catOne = data.news_category[0].category_name;
-    const catTow = data.news_category[1].category_name
-    const catThree = data.news_category[2].category_name
-    const catFour = data.news_category[3].category_name
-    const CatFive = data.news_category[4].category_name
-    const catSix = data.news_category[5].category_name
-    const catSeven = data.news_category[6].category_name
-    const catEight = data.news_category[7].category_name
-    console.log(catOne, catThree, catTow, catFour, CatFive, catSix, catSeven, catEight)
+    const categoryMenu = document.getElementById("all_category");
+    data.forEach(category => {
+        // console.log(category.category_name)
+        const a = document.createElement('a')
+        a.innerHTML =`
+        <a class="nav-link" href="#">${category.category_name}</a>
+        `;
+        categoryMenu.appendChild(a)
+    });
 }
-setAllMenu();
+
+setAllMenu()
